ab05ee2ba95bffa10e3394888efa72c0c68a68ad
Xandyhoss <xandyharrison@gmail.com>
2022-05-27 22:05:58 -0300

    Added transitions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-view />
+  <div class="animation-container">
+    <router-view v-slot="{ Component }">
+      <transition
+        enter-active-class="animate__animated animate__fadeInRight"
+        leave-active-class="animate__animated animate__fadeOutLeft"
+        mode="out-in"
+      >
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <script>
@@ -9,4 +19,13 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.animate__animated.animate__fadeInRight,
+.animate__animated.animate__fadeOutLeft {
+  --animate-duration: 0.6s;
+}
+.animation-container {
+  overflow-x: hidden;
+  background-color: var(--cta-1);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -7,5 +7,6 @@ import bootstrap from './bootstrap';
 import './assets/css/main.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
+import 'animate.css';
 
 createApp(App).use(store).use(router).use(bootstrap).mount('#app');
